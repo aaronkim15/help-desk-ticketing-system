5ef09d273d55a3cece9c4f942a6ab0c73e333408
ctlchan <christakleungchan@gmail.com>
2026-02-27 22:36:10 -0500

    Add link via table id to ticket details

--- js/history.js
+++ js/history.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
 function initData() {
     // TODO: get user ID from auth cookie/token
     const tickets = getUserTickets(4567, "customer");
+    const pastTickets = tickets.filter(ticket => ticket.status == "Resolved" || ticket.status == "Closed")
+
 
     console.log(tickets)
 
@@ -14,21 +16,26 @@ function initData() {
     const table = document.getElementById("ticketsTable");
     const body = document.getElementById("ticketsBody");
     
-    if (!tickets) {
+    if (tickets.length === 0) {
         empty.classList.remove("hidden"); 
         table.classList.add("hidden");
     }
     else {
-        tickets.forEach(ticket => {
+
+        pastTickets.forEach(ticket => {
             const row = document.createElement("tr");
 
             const id = document.createElement("td");
+            const idLink = document.createElement("a");
             const subject = document.createElement("td");
             const status = document.createElement("td");
             const priority = document.createElement("td");
             const date = document.createElement("td");
 
-            id.textContent = `#${ticket.id}`
+            idLink.textContent = `#${ticket.id}`
+            idLink.href = `../pages/ticket_detail.html?id=${ticket.id}`
+            id.appendChild(idLink)
+
             subject.textContent = ticket.subject;
             status.textContent = ticket.status;
             priority.textContent = ticket.priority;
